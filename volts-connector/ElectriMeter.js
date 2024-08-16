d0f360496134efd973caeaa96d937e3cc6f1406d
irumenov <ivaylo.rumenov2@dxc.com>
2024-08-16 11:01:20 +0300

    Created sceleton for Electric meter class

--- volts-connector/ElectriMeter.js
+++ volts-connector/ElectriMeter.js
@@ -68,7 +68,36 @@ export default class{
         client.connectRTUBuffered("COM3", { baudRate: 9600 });
         //Read
         getMeterValue(this.address)
-        //TODO call setMeter
+        const len2Data = getMeterValueLen2(this.address)
+        //TODO add activePowerData
+        setMeter(this.name,this.address,
+            len2Data[0], len2Data[1],len2Data[2],
+            len2Data[3],len2Data[4],len2Data[5],
+            len2Data[6],len2Data[7],len2Data[8],
+            len2Data[9],len2Data[10], len2Data[11], 
+            len2Data[12]
+        )
+        //Sent api
+        const postMeterData = JSON.stringify({
+            merterId: this.address,
+            voltagell1: len2Data[0].toFixed(2),
+            voltagell2: len2Data[1].toFixed(2),
+            voltagell3: len2Data[2].toFixed(2),
+            currentl1: len2Data[3].toFixed(4),
+            currentl2: len2Data[4].toFixed(4),
+            currentl3: len2Data[5].toFixed(4),
+            activepowerl1: len2Data[6].toFixed(2),
+            activepowerl2: len2Data[7].toFixed(2),
+            activepowerl3: len2Data[8].toFixed(2),
+            pfl1: len2Data[9].toFixed(6),
+            pfl2: len2Data[10].toFixed(6),
+            pfl3: len2Data[11].toFixed(6),
+            totalActivePpower: len2Data[12].toFixed(2),
+            totalActiveEnergyImportTariff1: 0,
+            totalActiveEnergyImportTariff2: 0,
+        });
+        await sleep(100);
+        await sendMerterDataRequestPost(postMeterData);
     }
 
 
@@ -107,9 +136,9 @@ export default class{
     }
 } 
 
-const getMeterValue = async (id) => {
+const getMeterValue = async (address) => {
     try {
-      await client.setID(id);
+      await client.setID(address);
       let val = await client.readInputRegisters(801, 4).then((res) => {
         return modbusRegistersToDouble(res.data);
       });
@@ -117,4 +146,21 @@ const getMeterValue = async (id) => {
     } catch (e) {
       return -1;
     }
-  };
+};
+
+const getMeterValueLen2 = async (address) => {
+    const addresses = [1, 3, 5, 13, 15, 17, 25, 27, 29, 37, 39, 41, 65]; //all addresses for len2
+    let allFoundAddressData = [];
+    for (let address of addresses) {
+      try {
+        await client.setID(address);
+        let val = await client.readInputRegisters(address, 2).then((res) => {
+          allFoundAddressData.push(decodeFloat(res.data));
+        });
+      } catch (e) {
+        return -1;
+      }
+    }
+    console.log("Len 2 Volt data", allFoundAddressData);
+    return allFoundAddressData;
+};
