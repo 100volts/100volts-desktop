7f40647595a75a29841f67d27624a9dda30009f5
irumenov <ivaylo.rumenov2@dxc.com>
2024-08-17 14:50:51 +0300

    Working on rework

--- volts-connector/electric/ElectriMeter.js
+++ volts-connector/electric/ElectriMeter.js
@@ -1,10 +1,10 @@
 import ModbusRTU from "modbus-serial";
-import getMeterValueLen2 from "./GetMeterValueLen2.js"
-import getMeterValueLen4 from "./GetMeterValueLen4.js"
-
+import getMeterValueLen2 from "../GetMeterValueLen2.js"
+import getMeterValueLen4 from "../GetMeterValueLen4.js"
 
 export default class{
-    constructor(
+/*
+  constructor(
     name,address,value,
     voltageL1,voltageL2,voltageL3,
     currentL1,currentL2,currentL3,
@@ -29,6 +29,7 @@ export default class{
         this.powerFactorL3=powerFactorL3;
         this.totActivePower=totActivePower;
     }
+        */
 
     getMeterByAddress(address,company){
         //TODO add api call
--- volts-connector/newindex.js
+++ volts-connector/newindex.js
@@ -10,11 +10,14 @@ import https from "https";
 import readline from "readline";
 import ModbusRTU from "modbus-serial";
 import XLSX from "xlsx";
-import ElectricMater from "./ElectricMater.js";
+import ElectricMater from "./electric/ElectricMater.js";
 
 //0 -Set company gy name
 let port;
 let companyName;
+let reqdata;
+let accesToken;
+
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 //const sleepALot = (ms = 120000) => new Promise((r) => setTimeout(r, ms));//this is 2 mins
 const sleepALot = (ms = 60000) => new Promise((r) => setTimeout(r, ms));
@@ -56,10 +59,48 @@ async function askForPort() {
     console.log(port);
 }
 
+//0.1- Auth user
 
+const postData = JSON.stringify({
+    email: "plamen@mail.com",
+    password: "12345678",
+  });
+  const options = {
+    hostname: "192.168.0.102",
+    port: 8081,
+    path: "/api/vi/auth/authenticate",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": Buffer.byteLength(postData),
+    },
+};
+async function sendPostRequest() {
+  return new Promise((resolve, reject) => {
+    const req = http.request(options, (res) => {
+      let data = "";
 
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
 
-  
+      res.on("end", () => {
+        try {
+          resolve(data);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
+
+    req.on("error", (e) => {
+      reject(`Problem with request: ${e.message}`);
+    });
+
+    req.write(postData);
+    req.end();
+  });
+}
 //1- read meter address list
 //2- Create classes for ech of the list
 //3- read from meter
